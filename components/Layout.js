fb75e896f173113736faed35bef7b557e5bb3324
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-28 19:36:12 -0600

    imported Head into Layout, was able to add title

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 const Layout = (props) => (
     <div>
+        <Head>
+            <title>NEXT | Playground</title>
+        </Head>
         <NavBar />
         {props.children}
         <Footer />
