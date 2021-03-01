5c7ef13c32eb708f3d31dfcc1adfea643ae47f62
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-28 19:21:55 -0600

    wrapped projects page in a Layout component

--- pages/projects.js
+++ pages/projects.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Link from "next/link"
+import Layout from "../components/Layout"
 
 const Projects = () => {
     return (
-        <div>
+        <Layout>
             <p>
             Projects
             </p>
             <Link href="/"><a>Home</a></Link>
-        </div>
+        </Layout>
     )
 }
 
