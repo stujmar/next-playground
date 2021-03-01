9f993c7f3eecdfe80858e5368b51dd507966134e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-28 19:26:38 -0600

    populated Team page and wrapped it in Layout comp

---
+++ pages/team.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Team = () => {
+    return (
+        <Layout>
+            Teams
+        </Layout>
+    )
+}
+
+export default Team;
