dd095257f38288d5fc34ad66f12491b799608ff4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-03-01 21:36:01 -0600

    trying different editor still breating my head against the problems

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link"
+import SunEditor from 'suneditor-react';
 import Layout from "../components/Layout"
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import MyEditor from "../components/MyEditor";
 
 
 const Calendar = () => {
+    const [text, setText] = useState('')
+
     return (
         <Layout>
             <p>
             Calendar
-
+            <MyEditor />
+         
             </p>
             <Link href="/"><a>Home</a></Link>
+            {text}
         </Layout>
     )
 }
