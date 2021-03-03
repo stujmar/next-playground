7968dd9d55888839fcc08819bc77e5bf41b72c5f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-03-02 21:16:42 -0600

    cleaned out all quill dependencies and imports got app running again

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
 import Link from "next/link"
-// import SunEditor from 'suneditor-react';
+import SunEditor from 'suneditor-react';
 import Layout from "../components/Layout"
-// import { CKEditor } from "@ckeditor/ckeditor5-react";
-// import MyEditor from "../components/MyEditor";
-// // import { Editor } from "../components/Editor";
-// import { Editor } from 'primereact/editor';
-import ReactQuill from 'react-quill'; // ES6
-import FormHtmlEditor from '../components/QuillWrapper.js'
+
 const Calendar = () => {
     const [text, setText] = useState('')
 
