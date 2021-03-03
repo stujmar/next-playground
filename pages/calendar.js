a077b43f8f504bd835c0a41c2bd453ea7900e970
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-03-02 21:04:35 -0600

    rebrought in Layout in calendar page along sidereact-quill

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link"
 // import SunEditor from 'suneditor-react';
-// import Layout from "../components/Layout"
+import Layout from "../components/Layout"
 // import { CKEditor } from "@ckeditor/ckeditor5-react";
 // import MyEditor from "../components/MyEditor";
 // // import { Editor } from "../components/Editor";
