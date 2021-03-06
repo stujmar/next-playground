98e5dbe3772b19b3087fb54f0feaed1ceb07c6b7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-03-05 22:04:59 -0600

    faked a active class NavLink in Next

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 const NavBar = () => {
+
+    const router = useRouter();
+    const activeStyle = "text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+    const passiveStyle = "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+
     return (
         <nav className="bg-gray-800">
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -38,10 +44,10 @@ const NavBar = () => {
             <div className="hidden sm:block sm:ml-6">
             <div className="flex space-x-4">
                 {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
-                <Link href="/"><a className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a></Link>
-                <Link href="/team"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a></Link>
-                <Link href="/projects"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a></Link>
-                <Link href="calendar"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a></Link>
+                <Link href="/"><a className={router.pathname == "/" ? activeStyle : passiveStyle }>Dashboard</a></Link>
+                <Link href="/team"><a className={router.pathname == "/team" ? activeStyle : passiveStyle }>Team</a></Link>
+                <Link href="/projects"><a className={router.pathname == "/projects" ? activeStyle : passiveStyle }>Projects</a></Link>
+                <Link href="calendar"><a className={router.pathname == "/calendar" ? activeStyle : passiveStyle }>Calendar</a></Link>
             </div>
             </div>
         </div>
