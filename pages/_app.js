b61ff5d16caa408671414001bb95ddf3c315b51b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-09 21:11:38 -0500

    [sm] added state to _app.js as a test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import '../styles/globals.css';
 // import '../public/quill.snow.css';
 // import '../node_modules/quill/dist/quill.snow.css';
 
 function MyApp({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
